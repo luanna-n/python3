eb630d54a8d00cb484b713832f44413e4f207d64
luanna-n <nayara.luanna@hotmail.com>
2021-11-18 14:14:21 -0300

    area + formats

--- tuto/area_calc.py
+++ tuto/area_calc.py
@@ -2,4 +2,10 @@
 
 radius = int(input("Entre com o raio do seu circulo (m): "))
 area = 3.142 * radius ** 2
-print("A area do seu círculo é de:", area)
+print("A area do seu círculo é de: ", area)
+
+#FORMAT METHOD
+print("A area do seu círculo é de: {:.2f}".format(area))
+
+#USING F-STRINGS
+print(f'A area do seu círculo é de: {area:.3f}')
