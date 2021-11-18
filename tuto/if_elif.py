04fff2cbbec9e8c3913023bb02ccb87a843b2407
luanna-n <nayara.luanna@hotmail.com>
2021-11-18 14:31:27 -0300

    if statements

---
+++ tuto/if_elif.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+
+if age < 10:
+    print("You are young")
+elif age < 40:
+    print("The fire in you is strong")
+else:
+    print("You are wise beyond doubt")
